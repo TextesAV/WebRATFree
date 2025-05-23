deec95bbe087c9fc66958bc86b3c1fdf28221798
TextesAV <krasivaya712@gmail.com>
2025-05-23 16:31:18 +0500

    Delete gorat directory

--- gorat/pkg/common/message.go
+++
@@ -1,7 +0,0 @@
-package common
-
-// Message defines the structure for communication between server and stub.
-type Message struct {
-	Type    string      `json:"type"`
-	Payload interface{} `json:"payload"`
-}
